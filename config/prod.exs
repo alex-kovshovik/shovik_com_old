21cb7c88a04c8deb4f04dcd7c856c5fa0b226754
Alex Kovshovik <alex@shovik.com>
2016-11-23 15:13:18 -0500

    Prepare app to run in production

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :shovik_com, ShovikCom.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "shovik.com", port: 8080],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
