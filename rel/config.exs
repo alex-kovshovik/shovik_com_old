79e5fcad6090d6bc2f0fbbeacc3d0e442b3b53c0
Alex Kovshovik <alex@shovik.com>
2017-01-03 10:59:14 -0500

    Dammit! They told me I was going to get hurt getting it to work... WHY???!!!

--- rel/config.exs
+++ rel/config.exs
@@ -8,6 +8,6 @@ environment :prod do
   set cookie: :"OYQ>8j$rWRU{R0ts|mtNU-V-i!1i|=MC.X+q9MR/KYfbFo6_c}+;~aF~.R5:%Ra;"
 end
 
-release :current_release do
+release :shovik_com do
   set version: current_version(:shovik_com)
 end
