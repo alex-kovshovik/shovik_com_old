b88411e357f2ca9286bca8456b72a694c0ae4019
Alex Kovshovik <alex@shovik.com>
2017-01-11 10:20:33 -0500

    Add disquss to blog posts

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ShovikCom.Mixfile do
 
   def project do
     [app: :shovik_com,
-     version: "0.0.6",
+     version: "0.0.7",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
