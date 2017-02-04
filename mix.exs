3a7b49e9b2ac359d86403bbbf4e61e39b45b9500
Alex Kovshovik <alex@shovik.com>
2017-02-03 20:57:31 -0500

    Add favicons of ALL sizes

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ShovikCom.Mixfile do
 
   def project do
     [app: :shovik_com,
-     version: "0.0.18",
+     version: "0.0.19",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
